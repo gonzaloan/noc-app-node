074d5c089add303c46acc0f181d2defd410ccead
Gonzalo <gigix@Gonzalos-MacBook-Air.local>
2024-02-15 16:25:46 -0300

    first usecase

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,18 @@
+import {CronService} from "./cron/cron-service";
+import {CheckService} from "../domain/use-cases/checks/check-service";
 
 export class Server {
     static start() {
         console.log('Server started');
+
+        CronService.createJob('*/5 * * * * *',
+            () => {
+                //new CheckService().execute('http://localhost:3001')
+                const url = 'https://www.google.com';
+                new CheckService(
+                    () => console.log(`${url} Service is Ok`),
+                    (error) => console.log(error)
+                ).execute(url);
+            });
     }
 }
